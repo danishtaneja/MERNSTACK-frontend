11a9720927fd02c9eadb6b9a667a79f7e4c5fc16
danishtaneja <84839707+danishtaneja@users.noreply.github.com>
2022-09-20 22:22:24 +1000

    Update Signup .js

--- src/components/Account/Signup.js
+++ src/components/Account/Signup.js
@@ -54,8 +54,8 @@ const Signup = () => {
                 <div className="col-lg-3 col-md-2"></div>
                 <div className="col-lg-6 col-md-8 login-box">
                     <div className="col-lg-12 login-key">
-                        {/* <i className="fa fa-user" aria-hidden="true"></i> */}
-                        <img src="https://seeklogo.com/images/G/girls-and-clothing-fashion-shop-logo-0620490E6A-seeklogo.com.png" className='img-thumbnail form-logo' alt="" />
+                        <i className="fa fa-user" aria-hidden="true"></i> 
+                        {/* <img src="https://seeklogo.com/images/G/girls-and-clothing-fashion-shop-logo-0620490E6A-seeklogo.com.png" className='img-thumbnail form-logo' alt="" /> */}
                     </div>
                     <div className="col-lg-12 login-title">
                        Fashion Shop Signup
